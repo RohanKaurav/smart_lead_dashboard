64f1d0a2f3ae42d38d05e8d1ab609ee651e294f8
RohanKaurav <rohankaurav05@gmail.com>
2026-05-17 19:28:01 +0530

    added cors functionlity

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,12 +7,16 @@ import apiRoutes from './routes/index';
 export function createApp(): Application {
   const app = express();
 
-  app.use(
-    cors({
-      origin: env.clientUrl,
-      credentials: true,
-    }),
-  );
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || origin.endsWith(".vercel.app")) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true
+  }))
   app.use(express.json());
 
   app.use('/api', apiRoutes);
